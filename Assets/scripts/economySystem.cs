003ac83f710bd4cf4918775d0cbc4507332422af
zzckyy <ZackyMM2080@gmail.com>
2026-07-23 15:34:31 +0700

    refactor: remove unused Start method from economySystem class

--- Assets/scripts/economySystem.cs
+++ Assets/scripts/economySystem.cs
@@ -4,12 +4,6 @@ public class economySystem : MonoBehaviour
 {
     public int Point;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
